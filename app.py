0ddd71c7b5915ba11fae0f86d10456ca8f58b0d1
FBMFAST <fbm101802@gmail.com>
2026-08-14 22:10:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -517,21 +517,21 @@ btn_registrar = st.form_submit_button("✅ Registrar Venta")
 
 if btn_registrar:
     st.session_state.ventas.append({
-                "producto": plato,
-                "monto": monto,
-                "metodo": metodo
-            })
-            try:
-                supabase.table("pedidos").insert({
-                    "producto": plato,
-                    "monto": monto,
-                    "metodo": metodo,
-                    "total": monto,
-                    "estado": "Pendiente"
-                }).execute()
-                st.success("¡Venta registrada con éxito en la caja!")
-            except Exception as e:
-                st.error(f"Error al guardar en Supabase: {e}")    
+        "producto": plato,
+        "monto": monto,
+        "metodo": metodo
+    })
+    try:
+        supabase.table("pedidos").insert({
+            "producto": plato,
+            "monto": monto,
+            "metodo": metodo,
+            "total": monto,
+            "estado": "Pendiente"
+        }).execute()
+        st.success("¡Venta registrada con éxito en la caja!")
+    except Exception as e:
+        st.error(f"Error al guardar en Supabase: {e}")
    # --- RESUMEN DE VENTAS DESDE SUPABASE PARA EL DUEÑO ---
 st.subheader("📊 Reporte de Caja en Vivo")
 
